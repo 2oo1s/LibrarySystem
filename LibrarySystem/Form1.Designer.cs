980c0bd2df5c915322ebf4f4a02fe1c6509c2cea
Joowon <leejoowon0817@gmail.com>
2023-03-28 00:34:25 +0900

    first upload

---
+++ LibrarySystem/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿
+namespace LibrarySystem
+{
+    partial class BookForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnModiBooks = new System.Windows.Forms.Button();
+            this.btnAddBooks = new System.Windows.Forms.Button();
+            this.txtPublisher = new System.Windows.Forms.TextBox();
+            this.txtIsbn = new System.Windows.Forms.TextBox();
+            this.txtPages = new System.Windows.Forms.TextBox();
+            this.txtBookName = new System.Windows.Forms.TextBox();
+            this.txtIdx = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.BookDataGridView = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.BookDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnRefresh);
+            this.groupBox1.Controls.Add(this.btnDelete);
+            this.groupBox1.Controls.Add(this.btnModiBooks);
+            this.groupBox1.Controls.Add(this.btnAddBooks);
+            this.groupBox1.Controls.Add(this.txtPublisher);
+            this.groupBox1.Controls.Add(this.txtIsbn);
+            this.groupBox1.Controls.Add(this.txtPages);
+            this.groupBox1.Controls.Add(this.txtBookName);
+            this.groupBox1.Controls.Add(this.txtIdx);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(21, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(487, 232);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "도서 추가/수정/삭제";
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(380, 190);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(94, 36);
+            this.btnRefresh.TabIndex = 13;
+            this.btnRefresh.Text = "새로고침";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(254, 190);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(94, 36);
+            this.btnDelete.TabIndex = 12;
+            this.btnDelete.Text = "삭제";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnModiBooks
+            // 
+            this.btnModiBooks.Location = new System.Drawing.Point(129, 190);
+            this.btnModiBooks.Name = "btnModiBooks";
+            this.btnModiBooks.Size = new System.Drawing.Size(94, 36);
+            this.btnModiBooks.TabIndex = 11;
+            this.btnModiBooks.Text = "수정";
+            this.btnModiBooks.UseVisualStyleBackColor = true;
+            this.btnModiBooks.Click += new System.EventHandler(this.btnModiBooks_Click);
+            // 
+            // btnAddBooks
+            // 
+            this.btnAddBooks.Location = new System.Drawing.Point(6, 190);
+            this.btnAddBooks.Name = "btnAddBooks";
+            this.btnAddBooks.Size = new System.Drawing.Size(94, 36);
+            this.btnAddBooks.TabIndex = 10;
+            this.btnAddBooks.Text = "추가";
+            this.btnAddBooks.UseVisualStyleBackColor = true;
+            this.btnAddBooks.Click += new System.EventHandler(this.btnAddBooks_Click);
+            // 
+            // txtPublisher
+            // 
+            this.txtPublisher.Location = new System.Drawing.Point(341, 84);
+            this.txtPublisher.Name = "txtPublisher";
+            this.txtPublisher.Size = new System.Drawing.Size(133, 28);
+            this.txtPublisher.TabIndex = 9;
+            // 
+            // txtIsbn
+            // 
+            this.txtIsbn.Location = new System.Drawing.Point(341, 29);
+            this.txtIsbn.Name = "txtIsbn";
+            this.txtIsbn.Size = new System.Drawing.Size(133, 28);
+            this.txtIsbn.TabIndex = 8;
+            // 
+            // txtPages
+            // 
+            this.txtPages.Location = new System.Drawing.Point(99, 145);
+            this.txtPages.Name = "txtPages";
+            this.txtPages.Size = new System.Drawing.Size(133, 28);
+            this.txtPages.TabIndex = 7;
+            // 
+            // txtBookName
+            // 
+            this.txtBookName.Location = new System.Drawing.Point(99, 84);
+            this.txtBookName.Name = "txtBookName";
+            this.txtBookName.Size = new System.Drawing.Size(133, 28);
+            this.txtBookName.TabIndex = 6;
+            // 
+            // txtIdx
+            // 
+            this.txtIdx.Location = new System.Drawing.Point(99, 36);
+            this.txtIdx.Name = "txtIdx";
+            this.txtIdx.Size = new System.Drawing.Size(133, 28);
+            this.txtIdx.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(273, 94);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 18);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "출판사";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(273, 32);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 18);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "ISBN";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "페이지";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "도서 이름";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NO";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.BookDataGridView);
+            this.groupBox2.Location = new System.Drawing.Point(21, 250);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(755, 181);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "도서 현황";
+            // 
+            // BookDataGridView
+            // 
+            this.BookDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.BookDataGridView.Location = new System.Drawing.Point(10, 26);
+            this.BookDataGridView.Name = "BookDataGridView";
+            this.BookDataGridView.RowHeadersWidth = 62;
+            this.BookDataGridView.RowTemplate.Height = 30;
+            this.BookDataGridView.Size = new System.Drawing.Size(734, 149);
+            this.BookDataGridView.TabIndex = 0;
+            this.BookDataGridView.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.BookDataGridView_CellDoubleClick);
+            // 
+            // BookForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "BookForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.BookDataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView BookDataGridView;
+        private System.Windows.Forms.TextBox txtBookName;
+        private System.Windows.Forms.TextBox txtIdx;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnModiBooks;
+        private System.Windows.Forms.Button btnAddBooks;
+        private System.Windows.Forms.TextBox txtPublisher;
+        private System.Windows.Forms.TextBox txtIsbn;
+        private System.Windows.Forms.TextBox txtPages;
+    }
+}
+
---
+++ LibrarySystem/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace LibrarySystem
+{
+    public partial class BookForm : Form
+    {
+        public static string dbInfo()
+        {
+            string _server = "127.0.0.1";
+            int _port = 3306;
+            string _database = "world";
+            string _id = "root";
+            string _pw = "0817";
+            string _conncectionAddress = string.Format("Server={0}; Port={1}; Database={2}; Uid={3}; Pwd={4}",
+                _server, _port, _database, _id, _pw);
+
+            return _conncectionAddress;
+        }
+
+        public void init()
+        {
+            Text = "도서 관리";
+            txtIdx.ReadOnly = true;
+        }
+
+        public BookForm()
+        {
+            InitializeComponent();
+            init();
+
+            string _connectionAddress = dbInfo();
+
+            try
+            {
+                using (MySqlConnection dbConn = new MySqlConnection(_connectionAddress))
+                {
+                    dbConn.Open();
+
+                    string selectBookQuery = string.Format("SELECT *FROM books;");
+                    MySqlCommand command1 = new MySqlCommand(selectBookQuery, dbConn);
+                    MySqlDataAdapter adapter1 = new MySqlDataAdapter(command1);
+                    DataTable datatable1 = new DataTable();
+                    adapter1.Fill(datatable1);
+                    BookDataGridView.DataSource = datatable1;
+
+                    dbConn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAddBooks_Click(object sender, EventArgs e)
+        {
+            string _connectionAddress = dbInfo();
+            if (txtIsbn.Text == "" || txtBookName.Text == "" || txtPublisher.Text == "" || txtPages.Text == "")
+            {
+                MessageBox.Show("도서 정보를 입력해 주세요!");
+            }
+            else
+            {
+                try
+                {
+                    using (MySqlConnection dbConn = new MySqlConnection(_connectionAddress))
+                    {
+                        dbConn.Open();
+
+                        string insertBookQuery = string.Format("INSERT INTO books (Isbn, BookName, Publisher, Page)" +
+                            "VALUES ('{0}','{1}','{2}','{3}');",
+                            txtIsbn.Text, txtBookName.Text, txtPublisher.Text, txtPages.Text);
+                        MySqlCommand command2 = new MySqlCommand(insertBookQuery, dbConn);
+
+                        if (command2.ExecuteNonQuery() != 1)
+                            MessageBox.Show("도서 정보 입력 실패!");
+                        else
+                            MessageBox.Show("ISBN: " + txtIsbn.Text + ", 도서 정보 입력 성공!");
+
+                        string selectBookQuery = string.Format("SELECT * FROM books;");
+                        MySqlCommand command1 = new MySqlCommand(selectBookQuery, dbConn);
+
+                        MySqlDataAdapter adapter1 = new MySqlDataAdapter(command1);
+                        DataTable datatable1 = new DataTable();
+                        adapter1.Fill(datatable1);
+                        BookDataGridView.DataSource = datatable1;
+
+                        dbConn.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                txtIdx.Text = "";
+                txtIsbn.Text = "";
+                txtBookName.Text = "";
+                txtPublisher.Text = "";
+                txtPages.Text = "";
+            }
+        }
+
+        private void BookDataGridView_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtIdx.Text = BookDataGridView.Rows[e.RowIndex].Cells[0].Value.ToString();
+            txtIsbn.Text = BookDataGridView.Rows[e.RowIndex].Cells[1].Value.ToString();
+            txtBookName.Text = BookDataGridView.Rows[e.RowIndex].Cells[2].Value.ToString();
+            txtPublisher.Text = BookDataGridView.Rows[e.RowIndex].Cells[3].Value.ToString();
+            txtPages.Text = BookDataGridView.Rows[e.RowIndex].Cells[4].Value.ToString();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            string _connectionAddress = dbInfo();
+            int idx = 0;
+            string Isbn = "";
+            if (txtIsbn.Text == "" || txtBookName.Text == "" || txtPublisher.Text == "" || txtPages.Text == "")
+            {
+                MessageBox.Show("도서 정보를 입력해 주세요!");
+            }
+            else
+            {
+                idx = int.Parse(txtIdx.Text.Trim());
+                Isbn = txtIsbn.Text.Trim();
+            }
+            try
+            {
+                using (MySqlConnection dbConn = new MySqlConnection(_connectionAddress))
+                {
+                    dbConn.Open();
+
+                    string deleteBookQuery = string.Format("DELETE FROM books WHERE Idx='{0}' AND Isbn='{1}';", idx, Isbn);
+                    MySqlCommand command3 = new MySqlCommand(deleteBookQuery, dbConn);
+
+                    if (command3.ExecuteNonQuery() != 1)
+                        MessageBox.Show("해당 도서 품목이 삭제가 안됨!");
+                    else
+                        MessageBox.Show("도서 품목이 삭제됨!");
+
+                    dbConn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            txtIdx.Text = "";
+            txtIsbn.Text = "";
+            txtBookName.Text = "";
+            txtPublisher.Text = "";
+            txtPages.Text = "";
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            string _connectionAddress = dbInfo();
+
+            try
+            {
+                using (MySqlConnection dbConn = new MySqlConnection(_connectionAddress))
+                {
+                    dbConn.Open();
+
+                    string selectBookQuery = string.Format("SELECT * FROM books;");
+
+                    MySqlCommand command4 = new MySqlCommand(selectBookQuery, dbConn);
+                    MySqlDataAdapter adapter4 = new MySqlDataAdapter(command4);
+                    DataTable datatable4 = new DataTable();
+                    adapter4.Fill(datatable4);
+                    BookDataGridView.DataSource = datatable4;
+
+                    dbConn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            txtIdx.Text = "";
+            txtIsbn.Text = "";
+            txtBookName.Text = "";
+            txtPublisher.Text = "";
+            txtPages.Text = "";
+        }
+
+        private void btnModiBooks_Click(object sender, EventArgs e)
+        {
+            string _connectionAddress = dbInfo();
+
+            int idx = 0;
+            string Isbn = "", BookName = "", Publisher = "", Page = "";
+
+            if (txtIsbn.Text == "" || txtBookName.Text == "" || txtPublisher.Text == "" || txtPages.Text == "")
+            {
+                MessageBox.Show("도서 정보를 입력해 주세요!");
+            }
+            else
+            {
+                idx = int.Parse(txtIdx.Text.Trim());
+                Isbn = txtIsbn.Text.Trim();
+                BookName = txtBookName.Text.Trim();
+                Publisher = txtPublisher.Text.Trim();
+                Page = txtPages.Text.Trim();
+            }
+            try
+            {
+                using (MySqlConnection dbConn = new MySqlConnection(_connectionAddress))
+                {
+                    string updateBooksQuery = string.Format("UPDATE books SET Isbn='{0}', BookName='{1}', Publisher='{2}', Page='{3}' WHERE idx={4};",
+                        Isbn, BookName, Publisher, Page, idx);
+
+                    dbConn.Open();
+
+                    MySqlCommand command5 = new MySqlCommand(updateBooksQuery, dbConn);
+
+                    if (command5.ExecuteNonQuery() != 1)
+                        MessageBox.Show("도서 정보 수정 실패!");
+                    else
+                        MessageBox.Show("도서 정보 수정 완료!");
+
+                    dbConn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            txtIdx.Text = "";
+            txtIsbn.Text = "";
+            txtBookName.Text = "";
+            txtPublisher.Text = "";
+            txtPages.Text = "";
+        }
+    }
+}
